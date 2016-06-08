f0f164548cea3c218be08a78995ff0850246b3d6
Sam Lau <esayemm@gmail.com>
2016-06-07 21:09:11 -0700

    what dont save these

--- main.tf
+++ main.tf
@@ -20,12 +20,12 @@ resource "null_resource" "etcd" {
   }
 }
 
-/* module "k8s_master" { */
-/*   source = "./k8s_master" */
-/*   ssh_fingerprint = "${var.ssh_fingerprint}" */
-/*   private_key = "${var.private_key}" */
-/*   etcd_ips = "${module.etcd.public_ips}" */
-/* } */
+module "k8s_master" {
+  source = "./k8s_master"
+  ssh_fingerprint = "${var.ssh_fingerprint}"
+  private_key = "${var.private_key}"
+  etcd_ips = "${module.etcd.public_ips}"
+}
 
 output "etcd_ips" { value = "${module.etcd.public_ips}" }
 /* output "k8s_master_ip" { value = "${module.k8s_master.public_ip}" } */
