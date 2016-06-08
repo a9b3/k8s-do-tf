31715af7e2f071d72ac5ecd42e5c86b81d9dfa18
Sam Lau <esayemm@gmail.com>
2016-06-08 11:32:37 -0700

    setup kubectl

--- readme.md
+++ readme.md
@@ -9,7 +9,9 @@
 
 Once you have those binaries installed you are ready to go
 
-## The Script
+## Get Started
+
+### Init cluster config
 
 Set two env variables when running `just_doit.sh` for the first time.
 
@@ -21,3 +23,24 @@ DO_TOKEN=footoken PRIVATE_KEY=~/.ssh/do_rsa ./just_doit.sh
 ```
 
 This will create terraform.tfvars
+
+### Spin up nodes 
+
+```sh
+terraform plan
+terraform apply
+```
+
+This will output the master node ip for setting up kubectl.
+
+### Setup kubectl
+
+```sh
+./setup_kubectl.sh <master node ip>
+```
+
+Now you should be able to use kubectl on your local machine to interact with the cluster.
+
+```sh
+kubectl cluster-info
+```
--- setup_kubectl.sh
+++ setup_kubectl.sh
@@ -1,6 +1,34 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 cd "$parent_path"
 
+echo ""
+
+if [ $CLEAN_SLATE ]; then
+  rm -rf ./certs/admin-key.pem
+  rm -rf ./certs/admin.csr
+  rm -rf ./certs/admin.pem
+fi
+
+if [ -e ./certs/admin-key.pem ]; then
+  echo "admin-key already exists using existing key ..."
+else
+  echo "Creating cluster admin keypair ..."
+  openssl genrsa -out ./certs/admin-key.pem 2048
+
+  openssl req -new -key ./certs/admin-key.pem \
+    -out ./certs/admin.csr \
+    -subj "/CN=kube-admin"
+
+  openssl x509 -req -in ./certs/admin.csr \
+    -CA ./certs/ca.pem \
+    -CAkey ./certs/ca-key.pem \
+    -CAcreateserial \
+    -out ./certs/admin.pem \
+    -days 365
+fi
+
+echo ""
+
 MASTER_IP=http://$1:8080
 CA_CERT=$(pwd)/certs/ca.pem
 ADMIN_KEY=$(pwd)/certs/admin-key.pem
